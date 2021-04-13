30eed7370991f0aa4a9f587c438d20cf680073aa
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2021-04-14 01:10:49 +0200

    Test page d'accueil

---
+++ scripts/initialize.js
@@ -0,0 +1,14 @@
+﻿// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCWi0EChm97lofJrhqBp6wRRtgQGKq8IEg",
+    authDomain: "infogares-f.firebaseapp.com",
+    projectId: "infogares-f",
+    storageBucket: "infogares-f.appspot.com",
+    messagingSenderId: "984551924335",
+    appId: "1:984551924335:web:247f2050a8ac4d68eb639a",
+    measurementId: "G-DRFJP2SFJ1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
