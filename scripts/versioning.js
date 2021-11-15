9516056586aa323a07328f58c82784a116ea5710
MaxALEau <maxaleau@gmail.com>
2021-11-15 10:49:55 +0100

    V1.1.1
    Fix versionning.js

--- scripts/versioning.js
+++ scripts/versioning.js
@@ -1,18 +1,34 @@
-const COMMITS_API = "https://api.github.com/repos/Absolument-Oui/InfoGare-Beta/commits?per_page=1000";
+const COMMITS_API = "https://api.github.com/repos/Absolument-Oui/InfoGare/commits?per_page=1000";
+const COMMITS_API_BETA = "https://api.github.com/repos/Absolument-Oui/InfoGare-Beta/commits?per_page=1000";
 var req = new XMLHttpRequest();
 
 function getVersion() {
-    req.open('GET', COMMITS_API, false);
-    req.onload = ((e) => {
-        var response = JSON.parse(req.response);
-        var item = response[0];
-        var message = item.commit.message;
-        console.log(message);
-        var version = message.split('\n');
-
-        document.getElementById('version').innerText = version[0];
-    });
-    req.send('');
+    if (location.pathname==="infogare.fr"){
+        req.open('GET', COMMITS_API, false);
+        req.onload = ((e) => {
+            var response = JSON.parse(req.response);
+            var item = response[0];
+            var message = item.commit.message;
+            console.log(message);
+            var version = message.split('\n');
+    
+            document.getElementById('version').innerText = version[0];
+        });
+        req.send('');
+    }
+    if (location.pathname==="beta.infogare.fr"){
+        req.open('GET', COMMITS_API_BETA, false);
+        req.onload = ((e) => {
+            var response = JSON.parse(req.response);
+            var item = response[0];
+            var message = item.commit.message;
+            console.log(message);
+            var version = message.split('\n');
+    
+            document.getElementById('version').innerText = version[0];
+        });
+        req.send('');
+    }
 }
 
 function getAllVersions() {
