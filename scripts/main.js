b68bddd04758c0043c967a4e7c5d800e90fde88e
Flo Youtube 54 <48404856+FloYoutube54@users.noreply.github.com>
2022-02-09 16:57:24 +0100

    version 1.2.3

--- scripts/main.js
+++ scripts/main.js
@@ -2055,7 +2055,7 @@ function loadTrains(user_id, id) {
             //document.getElementById('loader').style.display = 'none';
 
             scrollX();
-            checkDay();
+            //checkDay();
             clock();
         });
     }).catch((error) => {
